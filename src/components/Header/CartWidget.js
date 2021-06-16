8fe58b41e46d7f779ae7123dc3226bcc1e6b4997
MariaEmiliaWalter <eemi.walter@gmail.com>
2021-06-16 19:55:46 -0300

    sidebar-Right

--- src/components/Header/CartWidget.js
+++ src/components/Header/CartWidget.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { Button} from "semantic-ui-react";
+import {React, useState} from 'react';
+import {Icon,
+Button,
+  Grid,
+  Menu,
+  Sidebar} from "semantic-ui-react";
 import "./Header.css";
 
 function CartWidget() {
 
     // const counter = () =>{
     // --- AÑADIR ITEM A LIST CART + CONTADOR (Como en framer)---
+const [visible, setVisible] = useState(false);
+
+    const openSidebar = () => {
+      setVisible(!visible);
+    };
 
     return (
-      <Button
-        floated="right"
-        style={{ fontSize: "100%" }}
-        basic
-        color="grey"
-        circular
-        icon="shopping bag"
-        // visible={visible}
-        // onClick={openSidebar}
-      ></Button>
+      <div>
+        <Button
+          floated="right"
+          style={{ fontSize: "100%" }}
+          basic
+          color="grey"
+          circular
+          icon="shopping bag"
+          visible={visible}
+          onClick={openSidebar}
+        ></Button>
+        <Grid.Column>
+          <Sidebar
+            direction="right"
+            className="container"
+            as={Menu}
+            animation="overlay"
+            icon="labeled"
+            width="big"
+            onHide={() => setVisible(false)}
+            vertical
+            visible={visible}
+          >
+            <Menu.Item>
+              <div>Carrito de compras</div>
+            </Menu.Item>
+          </Sidebar>
+        </Grid.Column>
+      </div>
     );
 }
 

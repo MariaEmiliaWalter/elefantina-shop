18d8eb1078bae3db891db53edae8b1e16d46b4fe
MariaEmiliaWalter <eemi.walter@gmail.com>
2021-06-22 16:31:45 -0300

    Correcciones- funcion

--- src/components/itemList/ItemListContainer.js
+++ src/components/itemList/ItemListContainer.js
@@ -1,26 +1,17 @@
-import {React, useState, useEffect} from 'react';
+import React from 'react'
 import "./ItemListContainer.css";
 import ItemCard from './ItemCard';
-import { Card } from 'semantic-ui-react'
-//import img from './muselinas-de-bebe.svg';
 //DATA
-//import { Items } from './ItemsData';
+import { Items } from './ItemsData';
 
 
 function ItemListContainer() {
-
-    const [data, setData] = useState({});
-    useEffect(() => {
-        fetch("https://sheetdb.io/api/v1/ygrxgklerhj5t")
-          .then((res) => res.json())
-          .then((dataApi) => setData(dataApi));
-    }, [] );
-        
-    return (
-      <div className="displayGroup">
-        <ItemCard items={data} />
-      </div>
-    );
+  return (
+    <div className="displayGroup">
+      <ItemCard ItemsData={Items} />
+    </div>
+  )
 }
 
 export default ItemListContainer;
+
